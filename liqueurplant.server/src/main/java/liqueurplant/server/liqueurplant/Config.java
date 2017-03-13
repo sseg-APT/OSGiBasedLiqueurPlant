16047c68b334dc7e1fde1d813f71b797b152877c
John Buluba <buluba89@gmail.com>
2017-03-13 21:54:13 +0200

    Removed leshan server

--- liqueurplant.server/src/main/java/liqueurplant/server/liqueurplant/Config.java
+++
@@ -1,9 +0,0 @@
-package liqueurplant.server.liqueurplant;
-
-import org.eclipse.leshan.core.request.ContentFormat;
-
-
-class Config {
-    public static final ContentFormat CONTENT_FORMAT = ContentFormat.JSON;
-    public static final long TIMEOUT = 5000;
-}
