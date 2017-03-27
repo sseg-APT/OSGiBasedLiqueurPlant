2170d5c2b37391978b9aee1bcd80166e76cdca9f
Panos Bochalis <ece7858@upnet.gr>
2017-03-27 18:23:09 +0300

    Fixed changes on Silo Device.

--- liqueurplant.osgi.client/src/main/java/liqueurplant/osgi/client/SiloDevice.java
+++ liqueurplant.osgi.client/src/main/java/liqueurplant/osgi/client/SiloDevice.java
@@ -3,57 +3,32 @@ package liqueurplant.osgi.client;
 import liqueurplant.osgi.silo.controller.api.SiloCtrlIf;
 import org.eclipse.leshan.client.resource.LwM2mObjectEnabler;
 import org.eclipse.leshan.client.resource.ObjectsInitializer;
-import org.osgi.service.cm.ConfigurationAdmin;
-import org.osgi.service.cm.ConfigurationException;
-import org.osgi.service.cm.ManagedService;
-import org.osgi.service.component.annotations.*;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
-import java.util.Dictionary;
 import java.util.List;
-import java.util.Map;
 
 
 @Component(
         name = "liqueurplant.osgi.client",
-        immediate = true,
-        configurationPid = "liqueurplant.osgi.client.SiloDevice"
+        immediate = true
 )
-public class SiloDevice extends AbstractDevice implements ManagedService {
+public class SiloDevice extends AbstractDevice {
 
     SiloObject silo = new SiloObject();
-    private static final String PROP_HOST = "host";
-    private static final String PROP_PORT = "port";
 
-    private volatile String host;
-    private volatile Integer port;
-
-
-    public SiloDevice() {
-        super("silo", null);
-
-
-    }
+    public SiloDevice() { super("silo", null); }
 
     public SiloDevice(String endpoint, String[] args) {
         super(endpoint, args);
     }
 
-    @interface Config{
-        int port() default 8080;
-        String host();
-    }
-
     @Activate
-    public void activate(Map<String,Object> map) {
+    public void activate() {
         System.out.println("Silo device activated.");
-        System.out.println("Configuration " + map);
         new Thread(this).start();
         new Thread(silo).start();
-    }
-
-    @Modified
-    void modified(Config config) {
-        SiloObject.LOG.debug("Configuration " + config.host() + ":" + config.port());
     }
 
     @Override
@@ -72,33 +47,6 @@ public class SiloDevice extends AbstractDevice implements ManagedService {
         initializer.setInstancesForObject(SiloObject.modelId, silo);
         initializer.setClassForObject(FirmwareObject.modelId, FirmwareObject.class);
         return initializer;
-    }
-
-    @Override
-    public void updated(Dictionary<String, ?> dictionary) throws ConfigurationException {
-        SiloObject.LOG.info("Leshan server connection updated: " + dictionary);
-        if (dictionary == null) {
-            host = null;
-            port = null;
-            SiloObject.LOG.info("Unconfigured Leshan server connection.");
-        }
-        else {
-            host = (String) dictionary.get(PROP_HOST);
-            if (host == null) {
-                throw new ConfigurationException(PROP_HOST, "Mandatory field");
-            }
-            String portStr = (String) dictionary.get(PROP_PORT);
-            if (portStr == null) {
-                throw new ConfigurationException(PROP_PORT, "Mandatory field");
-            }
-            try {
-                port = Integer.parseInt(portStr);
-            } catch (NumberFormatException ex) {
-                throw new ConfigurationException(PROP_PORT, "Invalid number", ex);
-            }
-            SiloObject.LOG.info("Configured server connection for host: " + host + ", port: " + port);
-        }
-
     }
 
     @Reference
