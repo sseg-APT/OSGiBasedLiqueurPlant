c404e67d051b276bb2feaed6c46ee706dc288655
Panos Bochalis <ece7858@upnet.gr>
2017-04-05 20:04:05 +0300

    Logger at config manager

--- liqueurplant.osgi.client.config/src/main/java/liqueurplant/osgi/client/config/ConfigManager.java
+++ liqueurplant.osgi.client.config/src/main/java/liqueurplant/osgi/client/config/ConfigManager.java
@@ -7,6 +7,8 @@ import org.osgi.service.cm.ConfigurationException;
 import org.osgi.service.cm.ManagedService;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
@@ -21,12 +23,13 @@ import java.util.Hashtable;
 public class ConfigManager implements ManagedService {
 
     private ServiceRegistration service;
+    private Logger LOGGER = LoggerFactory.getLogger(ConfigManager.class);
 
     public ConfigManager(){}
 
     @Activate
     public void activate(BundleContext context){
-        System.out.println("ConfigManger started.");
+        LOGGER.info("ConfigManager started.");
         Dictionary props = new Hashtable();
         props.put(Constants.SERVICE_PID, "ConfigManagerService");
         service = context.registerService(ManagedService.class.getName(),
@@ -36,13 +39,12 @@ public class ConfigManager implements ManagedService {
     @Override
     public void updated(Dictionary<String, ?> properties) throws ConfigurationException {
         if (properties == null) {
-            // no configuration from configuration admin
-            // or old configuration has been deleted
-            System.out.println("ASDADAD");
+            LOGGER.warn("No configuration found.");
 
         } else {
             // apply configuration from config admin
-            System.out.println(properties.get("IP"));
+            LOGGER.info("IP: " + properties.get("IP"));
+            LOGGER.info("Port: "  + properties.get("port"));
 
         }
     }
